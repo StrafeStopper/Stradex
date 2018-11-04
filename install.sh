f89a9c2cb1c0598c060958cbbffb3196acfe6700
StrafeStopper <strafestopper@gmail.com>
2018-11-04 00:31:49 -0600

    updated installer

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo apt install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev
-make linux
+sudo apt install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev g++-mingw-w64-i686
+make
