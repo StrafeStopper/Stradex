eaab32960fde18d7c91e5ed521e3080e6dafc68f
StrafeStopper <strafestopper@gmail.com>
2018-11-07 13:55:38 -0700

    updated installer

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 echo "Installing dependincies..."
-sudo apt install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev g++-mingw-w64-i686
+sudo apt install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev g++-mingw-w64-i686 g++
 make
 echo "Use ./light to run the program after compiling"
