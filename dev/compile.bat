bcbb1c356c02d649420afba7613b94985940d392
StrafeStopper <strafestopper@gmail.com>
2019-12-12 01:12:00 -0700

    Added a compile script for windows till I figure out cmake ;-;

---
+++ dev/compile.bat
@@ -0,0 +1,2 @@
+
+g++ main.cpp init.cpp load.cpp game.cpp fileIO.cpp menu.cpp text.cpp -w -lmingw32 -static-libgcc -static-libstdc++ -LSDL2/i686-w64-mingw32/lib -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -o Stradex
