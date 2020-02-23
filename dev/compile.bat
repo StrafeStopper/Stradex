1ac590f060128fc2510824345fc08a1e041e294c
StrafeStopper <strafestopper@gmail.com>
2020-02-22 17:01:50 -0700

    updated some library stuff

--- dev/compile.bat
+++ dev/compile.bat
@@ -1,2 +1,2 @@
 
-g++ main.cpp init.cpp load.cpp game.cpp fileIO.cpp menu.cpp text.cpp -w -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -o Stradex
+g++ main.cpp init.cpp load.cpp game.cpp fileIO.cpp menu.cpp text.cpp -L\SDL2 -L\SDL2_image -L\SDL2_ttf -w -Wl,-V -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -o Stradex
