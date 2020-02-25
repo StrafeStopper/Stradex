a16719d7ac1177742fb05f3bfc15c11b0d3da471
StrafeStopper <strafestopper@gmail.com>
2020-02-24 17:46:15 -0700

    finished build_number script

--- dev/compile.bat
+++ dev/compile.bat
@@ -1,2 +1,3 @@
 
 g++ main.cpp init.cpp load.cpp game.cpp fileIO.cpp menu.cpp text.cpp -w -Wl,-V -LSDL2\i686-w64-mingw32\lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -o Stradex
+settings\build_number.bat
--- dev/settings/build_number.bat
+++ dev/settings/build_number.bat
@@ -1,8 +1,8 @@
-set /p build=<build_number.txt
-set add=1
-set /a "output=%build%+%add%"
-echo %output%
+@set /p build=<settings\build_number.txt
+@set add=1
+@set /a "output=%build%+%add%"
 
-echo %output% > "settings/build_number.txt"
 
-set output=0
+@echo %output% > "settings/build_number.txt"
+
+@set output=0
