0c012569936be63196f18b048918f6192dd37f69
StrafeStopper <strafestopper@gmail.com>
2024-09-02 00:02:27 -0600

    removed broken dependancies

--- dev/install.sh
+++ dev/install.sh
@@ -1,3 +1,3 @@
 echo "Installing dependincies..."
-sudo apt install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev g++-mingw-w64-i686 g++ 2to3 python2-minimal python2 dh-python python-is-python3
+sudo apt install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev g++-mingw-w64-i686 g++
 echo "Use ./light to run the program after compiling"
